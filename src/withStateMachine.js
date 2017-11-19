32eaefddc0c171349d3985e292b6ccddd98f40b4
Michele Bertoli <michele@berto.li>
2017-11-19 14:53:59 +0000

    devtools

--- src/withStateMachine.js
+++ src/withStateMachine.js
@@ -16,6 +16,27 @@ const withStateMachine = config => Component => {
       return { machineState: this.state.machineState }
     }
 
+    componentDidMount() {
+      if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+        this.devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect()
+        this.devTools.init({ machineState: this.state.machineState })
+        this.unsubscribe = this.devTools.subscribe(message => {
+          if (
+            message.type === 'DISPATCH' &&
+            message.payload.type === 'JUMP_TO_ACTION'
+          ) {
+            this.setState(JSON.parse(message.state))
+          }
+        })
+      }
+    }
+
+    componentWillUnmount() {
+      if (this.unsubscribe) {
+        this.unsubscribe()
+      }
+    }
+
     componentDidUpdate(prevProps, prevState) {
       if (
         this.instance.componentDidTransition &&
@@ -39,14 +60,24 @@ const withStateMachine = config => Component => {
         this.instance.componentWillTransition(action, payload)
       }
 
-      this.setState(prevState => ({
-        action,
-        componentState: { ...this.state.componentState, ...payload },
-        machineState: this.machine
-          .transition(prevState.machineState, action)
-          .toString(),
-        payload,
-      }))
+      this.setState(
+        prevState => ({
+          action,
+          componentState: { ...this.state.componentState, ...payload },
+          machineState: this.machine
+            .transition(prevState.machineState, action)
+            .toString(),
+          payload,
+        }),
+        () => {
+          if (this.devTools) {
+            this.devTools.send(action, {
+              componentState: this.state.componentState,
+              machineState: this.state.machineState,
+            })
+          }
+        }
+      )
     }
 
     render() {
